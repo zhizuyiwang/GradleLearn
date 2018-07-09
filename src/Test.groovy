e8bbe9fe113ae225052bb57c6d5a1fd0509581cf
hsf <1175432154@qq.com>
2018-07-10 02:36:53 +0800

    fvv

--- src/Test.groovy
+++ src/Test.groovy
@@ -1,1 +1,1 @@
-println("Hello Gradle")
+println ("Hello Gradle")
---
+++ src/variable/variablestudy.groovy
@@ -0,0 +1,13 @@
+package variable
+int x = 10
+println x.class
+
+double  y = 10.12
+println(y.class)
+
+def x_1 = 11
+def y_1 = 22.33
+def name = "android"
+println(x_1.class)
+println(y_1.class)
+println(name.class)
