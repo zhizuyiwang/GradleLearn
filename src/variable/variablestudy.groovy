e71951ee24365c7631352a56de27268a9efc4824
hsf <1175432154@qq.com>
2018-07-11 08:56:33 +0800

    fvv

--- src/variable/variablestudy.groovy
+++ src/variable/variablestudy.groovy
@@ -11,3 +11,14 @@ def name = "android"
 println(x_1.class)
 println(y_1.class)
 println(name.class)
+
+def name1 = '啊\'\'single'
+println name1.class
+def name2 = ''' jj jjjjkk  '''
+println name2
+println name2.class
+
+def job = "Student"
+def KK = "小明是 ${job}"
+println KK
+println KK.class
